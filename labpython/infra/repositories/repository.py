7af1d49c24970846a3d390836d13dac564d8e8e9
rodrigmars <rodrigmars@gmail.com>
2023-01-13 04:28:30 -0300

    Módulo repository refatorado

--- labpython/infra/repositories/repository.py
+++ labpython/infra/repositories/repository.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple, List, Callable
+from typing import Dict, Tuple, List, Callable, Any
 from labpython.infra.db.sqlite_db import create_connection, close_connection
 
 def repository(config:dict) -> Dict[str, Callable]:
@@ -7,21 +7,40 @@ def repository(config:dict) -> Dict[str, Callable]:
         print(message, error)
         pass
 
-    def execute(query, params):
+    def execute(query, params, mode) -> Any:
         
-        conn = None        
-        row_id: int = 0
+        conn = None
+
+        data: Any = None
+
         try:
-
+            
             conn = create_connection(config.get("PATH_DB_SQLITE", ""))
-
+            
             cur = conn.cursor()
 
             cur.execute(query, params)
 
-            conn.commit()
 
-            row_id = cur.rowcount
+            if mode in ("create","update", "delete"):
+
+                conn.commit()    
+
+                if "create" == mode:
+                    data = cur.rowcount
+
+            if mode == "one":
+
+                data = cur.fetchone()
+        
+            if mode == "all":
+                
+                data = cur.fetchall()
+            
+            if mode == "many":
+
+                data = cur.fetchmany(5)
+
 
         except Exception as ex:
 
@@ -31,24 +50,21 @@ def repository(config:dict) -> Dict[str, Callable]:
 
             close_connection(conn)
 
-            return row_id
-
-    def execute_query(query, params):
-        return "",
+            return data
 
     def create(query: str, params: dict) -> int:
-        return execute(query, params)
+        return execute(query, params, mode="create")
 
-    def find(query: str, params: dict):
-        return execute_query(query, params)
+    def find(query: str, params: dict) -> Tuple:
+        return execute(query, params, mode="one")
 
-    def all(query: str, params: dict):
-        return execute_query(query, params)
+    def all(query: str, params: dict) -> List:
+        return execute(query, params, mode="all")
 
     def edit(query: str, params: dict) -> None:
-        execute(query, params)
+        return execute(query, params, mode="update")
 
     def remove(query: str, params: dict) -> None:
-        execute(query, params)
+        return execute(query, params, mode="delete")
 
     return {"create": create, "find": find, "all": all, "edit": edit, "remove": remove}
