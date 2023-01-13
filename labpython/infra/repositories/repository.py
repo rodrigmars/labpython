874793fc257a8bfc2082b531dd47f7be12ccc63c
rodrigmars <rodrigmars@gmail.com>
2023-01-13 04:47:05 -0300

    Tipos criados para cursor

--- labpython/infra/repositories/repository.py
+++ labpython/infra/repositories/repository.py
@@ -1,3 +1,4 @@
+import enum
 from typing import Dict, Tuple, List, Callable, Any
 from labpython.infra.db.sqlite_db import create_connection, close_connection
 
@@ -7,7 +8,16 @@ def repository(config:dict) -> Dict[str, Callable]:
         print(message, error)
         pass
 
-    def execute(query, params, mode) -> Any:
+    class CurType(enum.Enum):
+        CREATE = 1
+        ONE = 2
+        ALL = 3
+        MANY = 4
+        UPDATE = 5
+        DELETE = 6
+
+    
+    def execute(query, params, cur_type:CurType) -> Any:
         
         conn = None
 
@@ -22,22 +32,22 @@ def repository(config:dict) -> Dict[str, Callable]:
             cur.execute(query, params)
 
 
-            if mode in ("create","update", "delete"):
+            if cur_type in (CurType.CREATE, CurType.UPDATE, CurType.DELETE):
 
                 conn.commit()    
 
-                if "create" == mode:
+                if CurType.CREATE == cur_type:
                     data = cur.rowcount
 
-            if mode == "one":
+            if cur_type == CurType.ONE:
 
                 data = cur.fetchone()
         
-            if mode == "all":
+            if cur_type == CurType.ALL:
                 
                 data = cur.fetchall()
             
-            if mode == "many":
+            if cur_type == CurType.MANY:
 
                 data = cur.fetchmany(5)
 
@@ -53,18 +63,18 @@ def repository(config:dict) -> Dict[str, Callable]:
             return data
 
     def create(query: str, params: dict) -> int:
-        return execute(query, params, mode="create")
+        return execute(query, params, CurType.CREATE)
 
     def find(query: str, params: dict) -> Tuple:
-        return execute(query, params, mode="one")
+        return execute(query, params, CurType.ONE)
 
     def all(query: str, params: dict) -> List:
-        return execute(query, params, mode="all")
+        return execute(query, params, CurType.ALL)
 
     def edit(query: str, params: dict) -> None:
-        return execute(query, params, mode="update")
+        return execute(query, params, CurType.UPDATE)
 
     def remove(query: str, params: dict) -> None:
-        return execute(query, params, mode="delete")
+        return execute(query, params, CurType.DELETE)
 
     return {"create": create, "find": find, "all": all, "edit": edit, "remove": remove}
