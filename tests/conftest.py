a4266ac5dce645b5ad7addd6bd2580d46d44bd2c
rodrigmars <rodrigmars@gmail.com>
2023-01-15 22:27:39 -0300

    BEGIN e COMMIT implementados para executescript

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ def create_tables(path_db:str) -> None:
 
     cur.executescript(
         """
+        BEGIN;
         DROP TABLE IF EXISTS PATIENTS;
         DROP TABLE IF EXISTS EXAMS;
         DROP TABLE IF EXISTS PATIENTS_EXAMS;
@@ -36,6 +37,7 @@ def create_tables(path_db:str) -> None:
             id_exams    INTEGER NOT NULL,
             FOREIGN KEY(id_patients) REFERENCES PATIENTS(id),
             FOREIGN KEY(id_exams) REFERENCES EXAMS(id));
+        COMMIT;
         """)
 
     conn.close()
