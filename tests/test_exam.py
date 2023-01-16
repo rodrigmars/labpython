e410f16e5ee2efb7a208c7878254b5c468175daa
rodrigmars <rodrigmars@gmail.com>
2023-01-15 22:22:31 -0300

    Função create_tables refatorada

--- tests/test_exam.py
+++ tests/test_exam.py
@@ -1,11 +1,8 @@
 
+def test_create(setup_patients):
 
-def setupe():
+    conn, config = setup_patients
+
+    print(conn, config)
+
         
-    query = """CREATE TABLE EXAMS(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                code TEXT NOT NULL UNIQUE,
-                description TEXT NOT NULL UNIQUE,
-                price REAL NOT NULL,
-                event_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-                last_update DATETIME NULL);"""
---
+++ tests/test_patient.py
@@ -0,0 +1,8 @@
+
+def test_create(setup_patients):
+
+    conn, config = setup_patients
+
+    print(conn, config)
+
+        
